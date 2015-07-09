69de9ef066355265560ac8f849e4a9ecee231453
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-07-09 11:30:33 +0200

    wtf

--- badges.js
+++ badges.js
@@ -3,5 +3,5 @@
 var labels = require('./index.js')
 
 labels.forEach(function (label) {
-  console.log('![' + label.name  + '](https://img.shields.io/badge/' + label.name + '-        -' + label.color + '.svg)')
+  console.log('![' + label.name + '](https://img.shields.io/badge/' + label.name + '-        -' + label.color + '.svg)')
 })
